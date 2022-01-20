d45d776ba6c06c200044c77ff6ac596734f0d2f3
karelvanoordt <63868555+karelvanoordt@users.noreply.github.com>
2022-01-20 08:44:03 -0500

    Add myEnumerable method and test cases

--- myList.rb
+++ myList.rb
@@ -1,13 +1,39 @@
+require_relative 'myEnumerable'
+
 class MyList
+
+    include MyEnumerable
     
-    def initialize(list)
+    def initialize(*list)
         @list = list
     end
 
     def each(&block)
-        @List.each do |item|
+        @list.each do |item|
           block.call(item) 
         end
     end
 
 end
+
+# Verify solution:
+# Create our list
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+puts list.all? {|e| e < 5}
+# true
+puts list.all? { |e| e > 5 }
+#false
+
+# Test #any?
+puts list.any? { |e| e == 2 }
+# true
+puts list.any? { |e| e == 5 }
+# false
+
+# Test #filter
+print list.filter(&:even?)
+# [2, 4]
+
+
