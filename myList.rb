ce1d75e6d7dfe37c346cb8976676322dc9ac1d07
karelvanoordt <63868555+karelvanoordt@users.noreply.github.com>
2022-01-20 08:17:11 -0500

    Create myList module

--- myList.rb
+++ myList.rb
@@ -1,1 +1,13 @@
-## Changes
+class MyList
+    
+    def initialize(list)
+        @list = list
+    end
+
+    def each(&block)
+        @List.each do |item|
+          block.call(item) 
+        end
+    end
+
+end
